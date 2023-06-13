6d1e6f79db3b871b8eaa767d6684e83c82e19725
Bersenev Sergey <s.s.bersenev@yandex.ru>
2023-06-13 17:11:07 +0500

    Коррекция HW#3: пытался изменить код, согласно рекомендациям преподавателя - промежуточный код

--- Navigation/Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Navigation/Profile/ProfileHeaderView.swift
@@ -8,27 +8,73 @@ import UIKit
 
 class ProfileHeaderView: UIView {
 
-    let showStatusButton: UIButton
-    let profileImage: UIImageView
-    let profileName: UILabel
-    let noteLabel: UILabel
-    
+    private lazy var profileImage: UIImageView = {
+        let image = UIImageView()
+        image.backgroundColor = .systemGreen
+        image.image = UIImage(named: "Hipster_Cat")
+        image.layer.cornerRadius = image.frame.height/2
+        image.layer.borderColor = UIColor.white.cgColor
+        image.layer.borderWidth = 3
+        image.clipsToBounds = true
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+
+    private lazy var showStatusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Show Status", for: .normal)
+        button.layer.cornerRadius = 4
+        button.layer.shadowRadius = 4
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.layer.shadowOffset = CGSize(width: 4, height: 4)
+        button.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        button.backgroundColor = .systemBlue
+        return button
+    }()
+
+    private lazy var profileName: UILabel = {
+        let label = UILabel
+        label.text = "Hipster Cat"
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.lineBreakMode = .byTruncatingMiddle
+        return label
+    }()
+
+
+    private var noteLabel: UILabel = {
+        let label = UILabel
+        label.text = "Waiting for something..."
+        label.adjustsFontSizeToFitWidth = true
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        return label
+    }()
+
+
+//    let showStatusButton: UIButton
+//    let profileImage: UIImageView
+//    let profileName: UILabel
+//    let noteLabel: UILabel
+
 
     override init (frame: CGRect){
-        profileImage = UIImageView()
-        profileImage.backgroundColor = .systemGreen
-        showStatusButton = UIButton()
-        showStatusButton.backgroundColor = .systemBlue
-        profileName = UILabel()
-        noteLabel = UILabel()
-
 
         super.init(frame: frame)
         addSubview(profileImage)
         addSubview(showStatusButton)
         addSubview(profileName)
         addSubview(noteLabel)
+
+        profileImage = UIImageView()
+        showStatusButton = UIButton()
+        profileName = UILabel()
+        noteLabel = UILabel()
+
     }
+
 
     required init?(coder: NSCoder){
         fatalError("Fatal Error")
@@ -43,13 +89,6 @@ class ProfileHeaderView: UIView {
             width: 100,
             height: 100
         )
-        profileImage.image = UIImage(named: "Hipster_Cat")
-        profileImage.layer.cornerRadius = profileImage.frame.height/2
-        profileImage.layer.borderColor = UIColor.white.cgColor
-        profileImage.layer.borderWidth = 3
-        profileImage.clipsToBounds = true
-        profileImage.contentMode = .scaleAspectFill
-
 
         showStatusButton.frame = CGRect(
             x: 16,
@@ -57,13 +96,6 @@ class ProfileHeaderView: UIView {
             width: frame.width - 32,
             height: 50
         )
-        showStatusButton.setTitle("Show Status", for: .normal)
-        showStatusButton.layer.cornerRadius = 4
-        showStatusButton.layer.shadowRadius = 4
-        showStatusButton.layer.shadowColor = UIColor.black.cgColor
-        showStatusButton.layer.shadowOpacity = 0.7
-        showStatusButton.layer.shadowOffset = CGSize(width: 4, height: 4)
-        showStatusButton.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
 
         profileName.frame = CGRect(
             x: 150,
@@ -71,11 +103,6 @@ class ProfileHeaderView: UIView {
             width: 200,
             height:20
         )
-        profileName.text = "Hipster Cat"
-        profileName.adjustsFontSizeToFitWidth = true
-        profileName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        profileName.textColor = .black
-        profileName.lineBreakMode = .byTruncatingMiddle
 
         noteLabel.frame = CGRect(
             x: 130,
@@ -83,12 +110,6 @@ class ProfileHeaderView: UIView {
             width: 200,
             height: 20
         )
-        noteLabel.text = "Waiting for something..."
-        profileName.adjustsFontSizeToFitWidth = true
-        noteLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        noteLabel.textColor = .gray
-
-
     }
 
     @objc func tapButton() {
